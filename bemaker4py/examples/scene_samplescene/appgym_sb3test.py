5cdb6f84bf6bb08c57c8f6ea80641f89a184778f
Gilzamir Gomes <54516662+gilzamir18@users.noreply.github.com>
2023-10-09 08:37:39 -0300

    Fix this example.

--- bemaker4py/examples/scene_samplescene/appgym_sb3test.py
+++ bemaker4py/examples/scene_samplescene/appgym_sb3test.py
@@ -1,7 +1,7 @@
 from bemaker.onnxutils import sac_export_to
 from bemaker.controllers import BasicGymController
 import BMEnv
-import gym
+import  gymnasium  as gym
 import numpy as np
 from stable_baselines3 import SAC
 from stable_baselines3.sac import MultiInputPolicy
@@ -13,23 +13,19 @@ bemaker Client Controller
 =======================
 This example controll a movable character in game (unity or godot).
 ''')
-
-
 model = SAC.load("sac_bemaker")
 
 sac_export_to("sac_bemaker", metadatamodel=env.controller.metadataobj)
 
-obs = env.reset()
+obs, info = env.reset()
 
 reward_sum = 0
 while True:
     action, _states = model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
+    obs, reward, done, truncated, info = env.step(action)
     reward_sum += reward
     env.render()
     if done:
       print("Testing Reward: ", reward_sum)
       reward_sum = 0
-      obs = env.reset()
-
-
+      obs, info = env.reset()
